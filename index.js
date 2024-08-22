5a813b7a54a87490eb6f42a3d20438bff79b080a
plsn33 <pedroxdw97@gmail.com>
2024-08-22 16:32:30 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+function somar (){
+    var nu1 = parseFloat(document.getElementById('n1').value);
+    var nu2 = parseFloat(document.getElementById('n2').value);
+
+    var soma = nu1 + nu2;
+
+    document.getElementById('Resultado').textContent = 'Te amo, você é a melhor namorada do mundo';
+}
